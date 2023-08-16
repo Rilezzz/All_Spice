d0e8b4107c049c9d96af77c66ed9f41d43499e85
Riley <riley@idahosuperiorcustoms.com>
2023-08-16 15:25:36 -0600

    Update

--- All_Spice.client/src/env.js
+++ All_Spice.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-h30qdrnxw3fag5mj.us.auth0.com'
+export const clientId = 'aYbeoXrXuRl54Cyznkvc2MO9W9lSvaf9'
+export const audience = 'classroom.com'
